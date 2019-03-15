ee99df56528c3df1f00616c51118129ab166c6e5
Malaka Alzoibi <malakaalzoibi@user3-233-190.wireless.utoronto.ca>
2019-03-14 22:55:13 -0400

    submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,14 +10,13 @@ function renderButtons(){
         a.addClass("emotion-btn");
         a.attr("data-name", emotions[i]);
         a.text(emotions[i]);
-        $("buttons-view").append(a);
+        $("#buttons-view").append(a);
     }
 }
 
 
 //create add emotion button 
 $("#add-emotion").on("click", function(event){
-    console.log("add button");
     event.preventDefault();
 
     var emotion = $("#emotion-input").val().trim();
@@ -27,5 +26,27 @@ $("#add-emotion").on("click", function(event){
     renderButtons();
 });
 
+//add gif
+function displayGifs(){
+	var emotion = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&limit=9&api_key=dc6zaTOxFJmzC";
 
+		$.ajax({url: queryURL, method: "GET"}).done(function (response) {
+			console.log(response.data);
+			// save results as a variable
+			var results = response.data;
+			// for loop goes through each gif and adds these variables
+			for (var i = 0; i < results.length; i++) {
+				// creates a generic div to hold the results
+				var gifDiv = $('<div class=gifs>');
+				var showGif = $('<img>');
+					showGif.attr('src', results[i].images.fixed_height_still.url);
+					
+				
+				gifDiv.append(showGif)
 
+				$("#view-gif").prepend(gifDiv);
+			}
+			
+		});
+}
